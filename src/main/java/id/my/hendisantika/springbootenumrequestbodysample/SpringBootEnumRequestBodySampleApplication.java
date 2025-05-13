9f3983905dedd3514fce1d29247b5d577b513c8d
hendisantika <hendisantika@yahoo.co.id>
2025-05-13 07:26:01 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootenumrequestbodysample/SpringBootEnumRequestBodySampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootenumrequestbodysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEnumRequestBodySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEnumRequestBodySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootenumrequestbodysample/SpringBootEnumRequestBodySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootenumrequestbodysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEnumRequestBodySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
