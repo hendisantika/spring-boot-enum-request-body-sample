4b85d0f8cdbb3e909cb417db254f6105a0fe0f10
hendisantika <hendisantika@yahoo.co.id>
2025-05-13 07:28:40 +0700

    feat: Add DemoEnum3🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootenumrequestbodysample/enums/DemoEnum3.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.springbootenumrequestbodysample.enums;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : enum-request-body-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/05/25
+ * Time: 07.28
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Getter
+public enum DemoEnum3 {
+
+    GOOD_GID("good_gid"),
+    HELLO_WORLD("helloWorld");
+
+    private final String text;
+
+    DemoEnum3(String text) {
+        this.text = text;
+    }
+}
