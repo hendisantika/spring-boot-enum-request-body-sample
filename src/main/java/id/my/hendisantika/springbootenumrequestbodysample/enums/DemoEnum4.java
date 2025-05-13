eb086a9425606ee90577990452091a1b7a68ca32
hendisantika <hendisantika@yahoo.co.id>
2025-05-13 07:29:29 +0700

    feat: Add DemoEnum4🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootenumrequestbodysample/enums/DemoEnum4.java
+++ src/main/java/id/my/hendisantika/springbootenumrequestbodysample/enums/DemoEnum4.java
@@ -43,3 +43,17 @@ public enum DemoEnum4 {
                 });
     }
 }
+
+/*
+Resolved [org.springframework.http.converter.HttpMessageNotReadableException:
+JSON parse error: Cannot construct instance of `java.util.ArrayList` (although at least one Creator exists):
+no String-argument constructor/factory method to deserialize from String value ('goodGid'); nested exception is com.fasterxml.jackson.databind.exc.MismatchedInputException:
+Cannot construct instance of `java.util.ArrayList` (although at least one Creator exists): no String-argument constructor/factory method to deserialize from String value ('goodGid')
+ at [Source: (PushbackInputStream); line: 3, column: 15] (through reference chain: dev.be.requestbody_enum_type.dto.request.Demo4Request["demoEnum"]->java.util.ArrayList[0])]
+
+
+ JSON parse error: Cannot deserialize value of type `java.util.ArrayList<dev.be.requestbody_enum_type.enums.DemoEnum4>` from String value (token `JsonToken.VALUE_STRING`);
+ nested exception is com.fasterxml.jackson.databind.exc.MismatchedInputException:
+ Cannot deserialize value of type `java.util.ArrayList<dev.be.requestbody_enum_type.enums.DemoEnum4>` from String value (token `JsonToken.VALUE_STRING`)
+ at [Source: (PushbackInputStream); line: 1, column: 22] (through reference chain: dev.be.requestbody_enum_type.dto.request.Demo4Request["demoEnum"])]
+ */
