8c59751e560fb5b99ac032a438577443dbecbabc
hendisantika <hendisantika@yahoo.co.id>
2025-05-15 12:56:31 +0700

    feat: Add Status Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootenumrequestbodysample/entity/Status.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.springbootenumrequestbodysample.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : enum-request-body-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/05/25
+ * Time: 12.54
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Status {
+    ACTIVATED, DEACTIVATED, SUSPENDED
+}
