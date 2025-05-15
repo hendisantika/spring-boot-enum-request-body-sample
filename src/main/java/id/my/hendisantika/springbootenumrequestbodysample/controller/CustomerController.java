babca8806c217f033d27dfab6388e53018a8b518
hendisantika <hendisantika@yahoo.co.id>
2025-05-15 14:03:58 +0700

    feat: Add findCustomers API🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootenumrequestbodysample/controller/CustomerController.java
+++ src/main/java/id/my/hendisantika/springbootenumrequestbodysample/controller/CustomerController.java
@@ -1,7 +1,17 @@
 package id.my.hendisantika.springbootenumrequestbodysample.controller;
 
+import id.my.hendisantika.springbootenumrequestbodysample.dto.response.CustomerResponse;
+import id.my.hendisantika.springbootenumrequestbodysample.entity.Status;
+import id.my.hendisantika.springbootenumrequestbodysample.repository.CustomerRepository;
+import id.my.hendisantika.springbootenumrequestbodysample.util.CustomerUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,6 +25,20 @@ import org.springframework.web.bind.annotation.RestController;
  * To change this template use File | Settings | File Templates.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/customers")
 public class CustomerController {
+    private final CustomerRepository customerRepository;
+
+    @GetMapping
+    public List<CustomerResponse> findCustomers(
+            @RequestParam(name = "name", required = false) String name,
+            @RequestParam(name = "status", required = false)
+            Status status) {
+        return customerRepository
+                .findCustomerByStatusAndName(status, name)
+                .stream()
+                .map(CustomerUtils::convertToCustomerResponse)
+                .collect(Collectors.toList());
+    }
 }
