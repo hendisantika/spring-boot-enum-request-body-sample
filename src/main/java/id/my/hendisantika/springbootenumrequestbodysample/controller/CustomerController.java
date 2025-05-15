5688637adb8c0f6b26ef72c352b0544eb8711b55
hendisantika <hendisantika@yahoo.co.id>
2025-05-15 13:09:32 +0700

    feat: Add CustomerController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootenumrequestbodysample/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.springbootenumrequestbodysample.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : enum-request-body-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/05/25
+ * Time: 13.09
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+}
