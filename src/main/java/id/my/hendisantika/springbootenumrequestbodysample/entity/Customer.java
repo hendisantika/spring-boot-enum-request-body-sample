c1f76b88e9016e44676a9b90cb8a6d49a49541ab
hendisantika <hendisantika@yahoo.co.id>
2025-05-15 12:56:45 +0700

    feat: Add Customer Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootenumrequestbodysample/entity/Customer.java
@@ -0,0 +1,40 @@
+package id.my.hendisantika.springbootenumrequestbodysample.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : enum-request-body-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/05/25
+ * Time: 12.50
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String email;
+    private String dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
